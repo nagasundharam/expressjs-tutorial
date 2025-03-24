95efd386325e377c612694dca29b49732326ecf2
nagasundharam <nagasundharam553@gmail.com>
2025-03-24 12:55:24 +0530

    Removed node_modules from gitracking

--- index.js
+++ index.js
@@ -7,84 +7,15 @@ const app = express();
 
 const PORT = 3000;
 
-//Define in a route
-// event controller in middle ware
-app.use((req,res,next)=> {
-    console.log("Start")
-    res.on('finish',()=> {
-        console.log("End");
-    })
-    next()
+// set EJS as the view engine
 
-})
-
-app.get('/error',()=>{
-    throw new Error('This is text error');
-})
-
-app.use((err,req,res,next)=>{
-    console.error(err.message);
-    res.send("Internal server Error");
-})
-
+app.set('view engine','ejs')
+app.use('/public',express.static('public'));
 app.get('/',(req,res)=>{
-    console.log('middle');
-    res.send('Hello Express');
-
+    const userName = 'nagasundhram'
+    res.render('index',{userName});
+  
 })
-
-app.get('/',(req,res)=>{
-    res.send('Hello naga');
-})
-
-
-app.use(express.json());
-// :username used to access the url name
-
-//post request
-app.post('/users',(req,res)=> {
-    const {name,email} = req.body;
-
-    res.json({
-        messase:` created a ${name} with  ${email}  successfully`
-    })
-})
-
-// put method
-
-app.put('/users/:id',(req,res)=>{
-    const userId = req.params.id;
-    const {name,email} = req.body;
-
-    res.json({
-        message:`user id${userId} is updated to  ${name} and ${email}`
-    })
-})
-
-// delete method
-
-app.delete('/users/:id',(req,res)=> {
-    const userId  = req.params.id;
-   
-
-    res.json({
-        message:`user id ${userId} is deleted successfully`
-    })
-})
-
-app.get('/things/:name/:id([0-9]{5})',(req,res)=> {
-     const { name , id} = req.params;
-
-     res.json({
-        id,name
-     });
-})
-app.get("*",(req,res)=> {
-    res.send('Sorry this is an invalid route');
-
-})
-app.use('/user',router);
-//
 
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
