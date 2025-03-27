25362a2230bc966d5f3a6acb3ba58b38c4b1371f
nagasundharam <nagasundharam553@gmail.com>
2025-03-27 11:55:14 +0530

    Learned : cookies mangaement and session management removing cookies and session details

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const cookieParser = require('cookie-parser');
 const express = require('express');
+const session = require('express-session');
 
 
 
 const app = express();
 
 app.use(cookieParser())
+
+app.use(session({
+    secret:"sample-secret",
+    resave:false,
+    saveUninitialized:false
+}))
 
 const PORT = 3000;
 
@@ -15,18 +22,25 @@ app.listen(PORT,()=>{
    
 
 app.get('/',(req,res)=> {
-    res.cookie('name','express-app',{maxAge:360000});
+   
 res.send("Hello");
 })
 
-app.get('/fetch',(req,res)=> {
-    console.log(req.cookies);
-    res.send("API CAlled");
+app.get('/visit',(req,res)=> {
+    if (req.session.page_views){
+        req.session.page_views++;
+        res.send(`You visited this page ${req.session.page_views} times`)
+    }
+    else {
+        req.session.page_views = 1;
+        res.send("Welcome to this page for the first time!")
+    }
 })
-app.get("/remove-cookie",(req,res)=> {
-    res.clearCookie('name');
-    res.send("removed cookie successfully");
+app.get('/remove-visit',(req,res)=> {
+    req.session.destroy();
+    res.send("Session removed successfully");
 })
+
 
 
 
