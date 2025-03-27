a09a41b0d9a0fdac404a4ea78ac462e879fe8238
nagasundharam <nagasundharam553@gmail.com>
2025-03-27 10:12:34 +0530

    LEarn:cookies usage,cookies deletion ,creating time period for cookie management

--- index.js
+++ index.js
@@ -1,87 +1,33 @@
+const cookieParser = require('cookie-parser');
 const express = require('express');
-const mongoose = require('mongoose');
-const connectDB = require('./config/db');
-const Person = require('./models/person.jsx');
+
+
 
 const app = express();
 
+app.use(cookieParser())
+
 const PORT = 3000;
-const startServer =  async ()=> {
-    try {
-        await connectDB();
-        console.log("connnected Successfully");
-        app.listen(PORT,()=>{
+
+app.listen(PORT,()=>{
             console.log(`Server is running on http://localhost:${PORT}`);
-    })
-    }
-    catch (error){
-        console.error("Database connection failed",error);
-        process.exit(1);
-
-    }
-
-}
-startServer();
+});
+   
 
 app.get('/',(req,res)=> {
+    res.cookie('name','express-app',{maxAge:360000});
 res.send("Hello");
 })
 
-app.post('/person',express.json(),async (req,res)=>{
-    console.log("1");
-   try {
-    const {name,age,email} = req.body;
-    const newPerson = new Person({
-        name,age,email}
-    )
-   
-    await newPerson.save();
-    console.log(newPerson);
-    res.send("Person Added");
-}catch(error){
-    res.send(error.errmsg);
-   
-}
+app.get('/fetch',(req,res)=> {
+    console.log(req.cookies);
+    res.send("API CAlled");
+})
+app.get("/remove-cookie",(req,res)=> {
+    res.clearCookie('name');
+    res.send("removed cookie successfully");
 })
 
-// updating data in mongodb
-
-app.put('/person',express.json(),async (req,res)=> {
-    const { id } = req.body;
-
-
-    if (!id){
-        return res.status(404).send('ID is required');
-
-    }
-    const personData = await Person.findById(id);
-     personData.age = 25;
-     personData.save();
-
-    console.log(personData);
-
-    res.send('Person Added');
-})
-app.delete('/person/:id',async (req,res)=> {
-    const {id}  = req.params;
-    await Person.findByIdAndDelete(id);
-    res.send('User Deleted');
-})
-
-
-
-
-
-
-
-// set EJS as the view engine
-
-
-
-app.get('/',(req,res)=>{
-    res.send('Hello wha ')
-   
- })
 
 
 // middle ware is an expressJs are functions that execute before the final request handler they can
